fe41eb0247193b2ca89f3f42b8f141fd7d2f61e6
mbackReserved <lowmustgoon@gmail.com>
2023-05-06 15:18:44 +0300

    finish hw

--- lesson_6/decorators.py
+++ lesson_6/decorators.py
@@ -2,6 +2,7 @@ import logging
 import logs.client_log
 import logs.server_log
 import sys
+import inspect
 
 
 print(sys.argv[0].find('client'))
@@ -15,6 +16,9 @@ else:
 def log(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        logger.debug(f'Зафиксировано обращение к функции {func.__name__} . Аргументы функции: {args}, {kwargs} ')
+        logger.debug(f'Зафиксировано обращение к функции {func.__name__} . Аргументы функции: {args}, {kwargs}.')
+        mainf = inspect.stack()[1][3]
+        if mainf != '<module>':
+            logger.debug(f'Вызов осуществлен из функции {mainf}')
         return result
     return wrapper
