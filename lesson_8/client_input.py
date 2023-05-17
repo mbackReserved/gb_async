a7da50b923c414e3f26f0e8fb0dca7bfbd7dfe38
mbackReserved <118825812+mbackReserved@users.noreply.github.com>
2023-05-17 07:43:31 +0300

    Les8 (#8)
    * set-up
    
    * add code to func
    
    * fsh

---
+++ lesson_8/client_input.py
@@ -0,0 +1,89 @@
+from socket import *
+import sys
+import json
+import time
+import logging
+import logs.client_log
+from decorators import log
+import threading
+
+client_logger = logging.getLogger('client_log')
+
+
+@log
+def resp_from_server(srv_resp):
+    try:   
+        msg = srv_resp['message']
+        status_code = srv_resp['response']
+        # print(f'Получено сообщение от сервера: "{msg}"')
+        client_logger.debug(f'Получено сообщение от сервера: "{msg}"')
+
+    except KeyError:
+         error = 'Не получены необходимые данные от сервера'
+         # print(error)
+         client_logger.error(error)
+         return error
+    return status_code
+
+
+def send_message_to_server(sock):
+    while True:
+        msg_to_srv = {
+            'action': 'presence',
+            'time': time.ctime(time.time()),
+            'user': {
+                'account_name': 'Vadim'
+            },
+            'mes': input('Введите: ')
+        }
+
+        msg_to_srv = json.dumps(msg_to_srv)
+        msg_to_srv = msg_to_srv.encode('utf-8')
+        sock.send(msg_to_srv)
+        #data = sock.recv(100000)
+
+        #return data
+
+
+
+@log
+def main():
+    try:
+        srv_ip = sys.argv[1]
+        srv_port = int(sys.argv[2])
+        client_logger.debug(f'Клиент запущен {srv_ip} : {srv_port}')
+    except (TypeError, IndexError):
+            srv_ip = '127.0.0.1'
+            srv_port = 7777
+            # print(f'Некорректно указаны порт и адрес. Присвоены стандартные значения. \n Порт: {srv_port}\n, IP-адрес: {srv_ip}')
+            client_logger.error(f'Некорректно указаны порт и адрес. Присвоены стандартные значения. \n Порт: {srv_port}\n, IP-адрес: {srv_ip}')
+    
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((srv_ip, srv_port))
+    # print('Соединение с сервером установлено')
+    client_logger.info('Соединение с сервером установлено')
+ 
+   
+
+    try:
+        send_message_to_server(sock)
+        dec_data = sock.recv(100000).decode('utf-8')
+        js_data = json.loads(dec_data)
+        resp_from_server(js_data)
+    except:
+        sys.exit(1)
+    
+    else:
+        receiver = threading.Thread(target=send_message_to_server, args=(sock, ), daemon=True)
+        receiver.start()
+
+        while True:
+            time.sleep(1)
+            if receiver.is_alive():
+                continue
+            break
+
+
+if __name__ == '__main__':
+    main()
+
