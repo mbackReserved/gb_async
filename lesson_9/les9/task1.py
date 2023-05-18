3003d46d424e328601a60a35d7410e10faa93947
mbackReserved <lowmustgoon@gmail.com>
2023-05-18 06:45:51 +0300

    task1

---
+++ lesson_9/les9/task1.py
@@ -0,0 +1,20 @@
+import ipaddress
+import subprocess
+
+
+def host_ping(ip_array):
+    for ip in ip_array:
+        try:
+            ip_address = ipaddress.ip_address(ip)
+        except ValueError:
+            print(f'{ip} нельзя преобразовать с помощью ip_address')
+        new_proc = subprocess.call(['ping', '-c', '2', ip], shell=False, stdout=subprocess.PIPE)
+        if new_proc:
+            print(f'{ip} - Узел недоступен')
+        else:
+            print(f'{ip} - Узел доступен')
+        
+
+if __name__ == '__main__':
+    ip_addrs = ['youtube.com', 'gb.ru', '16.10.24.154']
+    host_ping(ip_addrs)
